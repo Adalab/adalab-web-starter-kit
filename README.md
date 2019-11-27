9b7216bcf9f4601a05b96c54da9d4805ef7c50af
Miguel del Mazo <migueldelmazo@gmail.com>
2019-11-27 09:58:41 +0100

    Added api task to gulp

--- README.md
+++ README.md
@@ -1,35 +1,47 @@
 ![Adalab](_src/assets/images/logo-adalab-80px.png)
+
 # Adalab web starter kit
+
 Ahoy! Esta es nuestro Starter Kit en node/gulp para este primer contacto con el desarrollo web
 Incluye SCSS, un sistema de plantillas HTMl y un web server.
 
 ## Guía de inicio rápido
+
 Necesitarás instalar [Node.js](https://nodejs.org/) y [Gulp](https://gulpjs.com) para trabajar con este Starter Kit, luego:
+
 1. Descarga o clona el repositorio
 2. Instala las dependencias locales con `npm install`
 3. Arranca el kit con `gulp`
 
 ## Espera, ¿esto se hace siempre?
+
 > ### Solo una vez al principio en cada ordenador que utilicemos:
+
 - Instalamos node
 - Instalamos el comando de gulp de forma global para poder usarlo desde cualquier carpeta usando `npm install --global gulp-cli`
 
 > ### Cada vez que descarguemos o clonemos un repo:
+
 - `npm install` para instalar los paquetes necesarios para convertir Sass a CSS, minizarlo, etc.
 
 > ### Cada vez que estemos trabajando con nuestro código:
+
 - Desde nuestra terminal, ejecutamos el comando `gulp` para que realice la tarea por defecto, que en el caso del `gulpfile.js` que tenemos en adalab-web-starter-kit estará pendiente de nuestros archivos Sass, html y JavaScript y los compilará, minificará y/o recargará el servidor cada vez que hagamos un cambio
 
 ## Tareas de gulp incluidas
+
 ### Inicio de un web server para desarrollo
+
 ```
 npm start
 ```
+
 o lo que en este proyecto es lo mismo:
 
 ```
 gulp
 ```
+
 Lanza un webserver con BrowserSync y varios watchers estarán pendientes de los archivos SCSS/JS/HTML, en la carpeta **public/**, para recargar el navegador cuando se necesite.
 
 ### Versión lista para subir a producción
@@ -39,18 +51,23 @@ Para generar los ficheros para producción ejecuta:
 ```
 npm run docs
 ```
+
 o lo que en este proyecto es lo mismo:
+
 ```
 gulp docs
 ```
+
 En la carpeta **docs/** se generarán los CSS y JS minimizados y sin sourcemaps listos para subir al repo. A continuación súbelos al repo y activa en GitHub Pages la opción **master/docs/**, para que GitHub Pages sirva la página desde la carpeta **docs/**.
 
 ---
 
 Si quieres generar los ficheros listos para producción y además subirlos a GitHub directamente ejecuta el siguiente comando:
+
 ```
 npm run push-docs
 ```
+
 Este comando borra la carpeta **docs/**, la vuelve a generar, crea un commit con los nuevos ficheros y hace un `git push`, todo del tirón. ¿Cómo se te queda el cuerpo?. Si quieres saber cómo funciona échale un ojo al fichero `package.json`.
 
 ## Flujo de archivos con gulp
@@ -60,12 +77,16 @@ Estas tareas de gulp producen el siguiente flujo de archivos:
 ![Gulp flow](./gulp-flow.png)
 
 ## Estructura del proyecto
+
 Nuestro **gulpfile.js** usa un JSON de configuración con las rutas de los archivos a generar/vigilar.
 
 La estructura de carpetas tiene esta pinta:
+
 ```
 /
 `- _src
+   |- api
+   |  |- data.json // para crearnos un servidor de datos local
    |- assets
    |  |- icons
    |  |- images
@@ -79,19 +100,25 @@ La estructura de carpetas tiene esta pinta:
 ```
 
 ## HTML
+
 Viene incluído el paquete [**gulp-html-partial**](https://www.npmjs.com/package/gulp-html-partial) que nos va a permitir tener un sistema de plantillas html
 
 ## Imágenes e iconos
-Tenemos en **_src/** una carpeta para las imágenes del proyecto y una para los iconos como el favicon o los iconos de dispositivos móviles. Estos últimos se generan en la raíz de las carpetas **public/** y **docs/**
+
+Tenemos en **\_src/** una carpeta para las imágenes del proyecto y una para los iconos como el favicon o los iconos de dispositivos móviles. Estos últimos se generan en la raíz de las carpetas **public/** y **docs/**
 
 ## CSS
+
 Viene incluído el paquete [**gulp-combine-mq**](https://www.npmjs.com/package/gulp-combine-mq) que agrupa todas las mediaqueries al final del documento css.
 
 ## JS
+
 Podemos usar parciales de JS: en el JSON de configuración, **config.json** especificamos los archivos JS que utilizamos y en el orden que deben procesarse.
 
 ## ¿Cómo actualizo si tengo una versión anterior?
-En principio puedes descargar todos los archivos fuera de **_src/** y sustituir los de tu proyecto. Además deberías replicar la estructura de carpetas dentro de **_src/**.
+
+En principio puedes descargar todos los archivos fuera de **\_src/** y sustituir los de tu proyecto. Además deberías replicar la estructura de carpetas dentro de **\_src/**.
 
 ## Falta algo?
+
 Echas de menos que el kit haga algo en concreto? Pidelo sin problema a través de los Issues o si te animas a mejorarlo mándanos un PR :)
--- gulpfile.js
+++ gulpfile.js
@@ -1,200 +1,245 @@
 'use strict';
 
 const autoprefixer = require('gulp-autoprefixer');
-const browserSync  = require('browser-sync');
-const combineMq    = require('gulp-combine-mq');
-const concat       = require('gulp-concat');
-const config       = require('./config.json');
-const del          = require('del');
-const gulp         = require('gulp');
-const htmlPartial  = require('gulp-html-partial');
-const notify       = require('gulp-notify');
-const plumber      = require('gulp-plumber');
-const sass         = require('gulp-sass');
-const sourcemaps   = require('gulp-sourcemaps');
-const uglify       = require('gulp-uglify-es').default;
-
+const browserSync = require('browser-sync');
+const combineMq = require('gulp-combine-mq');
+const concat = require('gulp-concat');
+const config = require('./config.json');
+const del = require('del');
+const gulp = require('gulp');
+const htmlPartial = require('gulp-html-partial');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify-es').default;
 
 // > Dev tasks
 // >> Delete Public folder
 gulp.task('clean', del.bind(null, ['public']));
 
-
-
 // >> Process HTML files
 gulp.task('html', function(done) {
-  gulp.src(config.html.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-    .pipe(htmlPartial({
-      basePath: config.html.partials
-    }))
+  gulp
+    .src(config.html.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(
+      htmlPartial({
+        basePath: config.html.partials
+      })
+    )
     .pipe(gulp.dest(config.html.dest));
   done();
 });
 
-
-
 // >> Process SCSS files (extended + sourcemaps +  autoprefixer)
 gulp.task('styles', function(done) {
-  gulp.src(config.scss.src)
+  gulp
+    .src(config.scss.src)
     .pipe(sourcemaps.init())
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-    .pipe(sass({
-      outputStyle: 'extended',
-    }))
-    .pipe(combineMq({
-      beautify: true
-    }))
-    .pipe(autoprefixer({
-      browsers: [
-        'last 2 versions',
-        'ie >= 10'
-      ],
-      cascade: false
-    }))
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(
+      sass({
+        outputStyle: 'extended'
+      })
+    )
+    .pipe(
+      combineMq({
+        beautify: true
+      })
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 10'],
+        cascade: false
+      })
+    )
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.scss.dest))
-    .pipe(browserSync.reload({ stream:true }));
+    .pipe(browserSync.reload({ stream: true }));
   done();
 });
 
-
-
 // >> Concatenate JS files with sourcemaps
-gulp.task('scripts', function(done){
-  gulp.src(config.js.src)
+gulp.task('scripts', function(done) {
+  gulp
+    .src(config.js.src)
     .pipe(sourcemaps.init())
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
     .pipe(concat('main.js'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.js.dest))
-    .pipe(browserSync.reload({ stream:true }));
+    .pipe(browserSync.reload({ stream: true }));
   done();
 });
 
-
-
 // >> Copy image files
 gulp.task('images', function(done) {
-  gulp.src(config.images.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+  gulp
+    .src(config.images.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
     .pipe(gulp.dest(config.images.dest));
   done();
 });
 
-
-
-// >> Copy icon files
-gulp.task('icons', function(done) {
-  gulp.src(config.icons.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-    .pipe(gulp.dest(config.icons.dest));
+// >> Copy api files
+gulp.task('api', function(done) {
+  gulp.src(config.api.src).pipe(gulp.dest(config.api.dest));
   done();
 });
 
-
+// >> Copy icon files
+gulp.task('icons', function(done) {
+  gulp
+    .src(config.icons.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(gulp.dest(config.icons.dest));
+  done();
+});
 
 // > Production Tasks
 // > Delete Public folder
 gulp.task('clean-dist', del.bind(null, ['docs']));
 
-
-
 // >> Process HTML files
 gulp.task('html-dist', function(done) {
-  gulp.src(config.html.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-    .pipe(htmlPartial({
-      basePath: config.html.partials
-    }))
+  gulp
+    .src(config.html.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(
+      htmlPartial({
+        basePath: config.html.partials
+      })
+    )
     .pipe(gulp.dest(config.html.dist));
   done();
 });
 
-
-
 // >> Process SCSS files (compressed + autoprefixer)
 gulp.task('styles-dist', function(done) {
-  gulp.src(config.scss.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-    .pipe(sass({
-      outputStyle: 'compressed',
-    }))
-    .pipe(combineMq({
-      beautify: false
-    }))
-    .pipe(autoprefixer({
-      browsers: [
-        'last 2 versions',
-        'ie >= 10'
-      ],
-      cascade: false
-    }))
+  gulp
+    .src(config.scss.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(
+      sass({
+        outputStyle: 'compressed'
+      })
+    )
+    .pipe(
+      combineMq({
+        beautify: false
+      })
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 10'],
+        cascade: false
+      })
+    )
     .pipe(gulp.dest(config.scss.dist));
   done();
 });
 
-
-
 // >> Concatenate and minify JS files w/o sourcemaps
-gulp.task('scripts-dist', function(done){
-  gulp.src(config.js.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+gulp.task('scripts-dist', function(done) {
+  gulp
+    .src(config.js.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
     .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(gulp.dest(config.js.dist));
   done();
 });
 
-
+// >> Copy image files
+gulp.task('api-dist', function(done) {
+  gulp.src(config.api.src).pipe(gulp.dest(config.api.dist));
+  done();
+});
 
 // >> Copy image files
 gulp.task('images-dist', function(done) {
-  gulp.src(config.images.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+  gulp
+    .src(config.images.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
     .pipe(gulp.dest(config.images.dist));
   done();
 });
 
-
-
 // >> Copy icon files
 gulp.task('icons-dist', function(done) {
-  gulp.src(config.icons.src)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+  gulp
+    .src(config.icons.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
     .pipe(gulp.dest(config.icons.dist));
   done();
 });
 
-
-
 // > Watchers + BrowserSync server
-gulp.task('default', gulp.series(['clean','html', 'styles','scripts', 'images', 'icons'], function(done) {
-  browserSync.init({
-    server : {
-      baseDir: './public/'
+gulp.task(
+  'default',
+  gulp.series(
+    ['clean', 'html', 'styles', 'scripts', 'images', 'api', 'icons'],
+    function(done) {
+      browserSync.init({
+        server: {
+          baseDir: './public/'
+        }
+      });
+      gulp.watch(config.watch.html, gulp.series(['html', 'bs-reload']));
+      gulp.watch(config.images.src, gulp.series(['images', 'bs-reload']));
+      gulp.watch(config.api.src, gulp.series(['api', 'bs-reload']));
+      gulp.watch(config.icons.src, gulp.series(['icons', 'bs-reload']));
+      gulp.watch(config.scss.src, gulp.series('styles'));
+      gulp.watch(config.js.src, gulp.series(['scripts', 'bs-reload']));
+      done();
     }
-  });
-  gulp.watch(config.watch.html, gulp.series(['html', 'bs-reload']));
-  gulp.watch(config.images.src, gulp.series(['images', 'bs-reload']));
-  gulp.watch(config.icons.src, gulp.series(['icons', 'bs-reload']));
-  gulp.watch(config.scss.src, gulp.series('styles'));
-  gulp.watch(config.js.src, gulp.series(['scripts', 'bs-reload']));
-  done();
-}));
-
-
+  )
+);
 
 // > Build a production-ready version of your proyect
-gulp.task('docs', gulp.series(['clean-dist','html-dist','styles-dist','scripts-dist', 'images-dist', 'icons-dist'], function(done) {
-  console.log('🦄 Build OK!');
-  done();
-}));
-
-
+gulp.task(
+  'docs',
+  gulp.series(
+    [
+      'clean-dist',
+      'html-dist',
+      'styles-dist',
+      'scripts-dist',
+      'images-dist',
+      'api-dist',
+      'icons-dist'
+    ],
+    function(done) {
+      // eslint-disable-next-line no-console
+      console.log('🦄 Build OK!');
+      done();
+    }
+  )
+);
 
 // > Recarga las ventanas del navegador
-gulp.task('bs-reload', function (done) {
+gulp.task('bs-reload', function(done) {
   browserSync.reload();
   done();
 });
